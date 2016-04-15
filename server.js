4241eec61fbf53b8524e46c69d863a5753ad5f23
Daniel Holmlund <agnathan@gmail.com>
2016-04-15 14:14:29 +0200

    Removed the configuration file

--- server.js
+++ server.js
@@ -21,7 +21,6 @@
  * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-var config = require("./config.json");
 var _ = require("lodash");
 
 // Require MQTT and setup the connection to the broker
